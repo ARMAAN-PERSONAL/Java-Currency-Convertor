f07e2a4f2bdfe91aac977709992bff9082633566
Armaan Singh <arma0017@algonquinlive.com>
2024-09-04 14:48:37 -0400

    Signed-off-by: Armaan Singh <arma0017@algonquinlive.com>

--- CurrencyConvertor/src/ConvertorApps.java
+++ CurrencyConvertor/src/ConvertorApps.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /**
  * Driver of the program
  */
-public class ConvertorApp {
+public class ConvertorApps {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
